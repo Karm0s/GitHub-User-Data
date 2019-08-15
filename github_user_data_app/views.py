e6bf86c1e1b895fff76bc0284b50faffbf7349bd
PC\Karm0s <bouchenna.yanis@protonmail.com>
2019-08-15 13:59:05 +0100

    Django app setup

--- github_user_data_app/views.py
+++ github_user_data_app/views.py
@@ -1,3 +1,17 @@
+from django.shortcuts import HttpResponse, render
 from django.shortcuts import render
+from django.views.generic import View
 
+import requests
+import json
 # Create your views here.
+
+class IndexView(View):
+    template_name = 'index.html'
+
+    def get(self, request):
+
+        res = requests.get('https://api.github.com/users/Karm0s')
+
+
+        return HttpResponse(res)
